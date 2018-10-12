0d8a50fd94e42416676364b5ee2a19a07a735acd
quanquan <1239976927@qq.com>
2018-10-12 23:37:58 +0800

    购物车

--- src/scripts/controllers/admin.js
+++ src/scripts/controllers/admin.js
@@ -2,6 +2,9 @@ import adminTpl from "../views/admin.html"
 
 const render = ()=>{
     $("#root").html(adminTpl)
+    $(".ad-header i").on("tap",()=>{
+        location.hash="#position";
+    })
 }
 
 export default {
