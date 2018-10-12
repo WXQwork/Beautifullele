bfa2065b737f5097c987ca5a390241e949f82ba7
quanquan <1239976927@qq.com>
2018-10-12 12:14:48 +0800

    iconçª更新

--- src/scripts/controllers/profile.js
+++ src/scripts/controllers/profile.js
@@ -2,6 +2,9 @@ import profileTpl from '../views/profile.html'
 
 const render = () => {
   $('#root').html(profileTpl)
+  $(".laye-back i").on("tap",()=>{
+    location.hash="#position";
+  })
 }
 
 export default {
