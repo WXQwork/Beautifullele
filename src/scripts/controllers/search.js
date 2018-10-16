b1255a1fc4d9e4edd2bb64d77e54422e9b39df80
zhuzehui <1085055114@qq.com>
2018-10-16 22:02:52 +0800

    tijiao

--- src/scripts/controllers/search.js
+++ src/scripts/controllers/search.js
@@ -7,18 +7,28 @@ const render = () => {
   $("header").remove()
   $('main').html(searchTpl)
   scrollLeft()
+  scrollRight()
+  myEvent()
 }
 
 const scrollLeft = () => {
   let scroll = new BScroll('.class-mid-left', {
     scrollY: true
   })
-  scroll.on('scrollEnd', () => {
-    this.scrollTo(0, 0, 500)
+}
+
+const scrollRight = () => {
+  let scroll = new BScroll('.class-mid-right', {
+    scrollY: true
   })
 }
 
-
+const myEvent = () => {
+  $('.class-mid-left ul li').on('tap', function () {
+    $(this).addClass('current').siblings().removeClass('current')
+    $('.class-mid-right .content>div').eq($(this).index()).css('display', 'block').siblings().css('display', 'none')
+  })
+}
 
 export default {
   render
