103be728b675cfde819491a07b06cbcee10204bb
zhuzehui <1085055114@qq.com>
2018-10-16 21:17:36 +0800

    tijiao

--- src/scripts/controllers/search.js
+++ src/scripts/controllers/search.js
@@ -1,11 +1,24 @@
 import searchTpl from '../views/search.html'
 import homeCon from "./home"
+// import BScroll from '../../libs/bscroll.min.js'
 
 const render = () => {
   homeCon.render()
   $("header").remove()
   $('main').html(searchTpl)
+  scrollLeft()
 }
+
+const scrollLeft = () => {
+  let scroll = new BScroll('.class-mid-left', {
+    scrollY: true
+  })
+  scroll.on('scrollEnd', () => {
+    this.scrollTo(0, 0, 500)
+  })
+}
+
+
 
 export default {
   render
